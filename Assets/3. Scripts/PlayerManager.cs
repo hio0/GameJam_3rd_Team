a59e09ff730cce32ad4212852a8ab03321353330
hio0 <hioohio1234@naver.com>
2026-07-17 10:36:11 +0900

    sf

--- Assets/3. Scripts/PlayerManager.cs
+++ Assets/3. Scripts/PlayerManager.cs
@@ -1,9 +1,32 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    public static PlayerManager player;
+
+    public bool isfixedScene;
+
+    public event Action p1_AMove;
+    public event Action p1_DMove;
+    public event Action p2_LeftMove;
+    public event Action p2_RightMove;
+
+    private void Awake()
+    {
+        if(player == null)
+        {
+            player = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +36,25 @@ public class PlayerManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(isfixedScene)
+        {
+            if (Input.GetKey(KeyCode.A))
+            {
+                p1_AMove?.Invoke();
+            }
+            if(Input.GetKey(KeyCode.D))
+            {
+                p1_DMove?.Invoke();
+            }
+
+            if(Input.GetKey(KeyCode.LeftArrow))
+            {
+                p2_LeftMove?.Invoke();
+            }
+            if (Input.GetKey(KeyCode.RightArrow))
+            {
+                p2_RightMove?.Invoke();
+            }
+        }
     }
 }
